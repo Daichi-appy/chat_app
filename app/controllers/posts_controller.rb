849a4f8c7f2c44449a4fb14e39d833053c34b533
daichi.aoki <daichi.es47@gmail.com>
2021-06-22 01:03:30 +0900

    投稿機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,32 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @posts = Post.all
+    @post = Post.new
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.create(user_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path, alert: "投稿を削除しました"
+  end
+
+  private
+    def user_params
+      params.require(:post).permit(:title, :body)
+    end
 end
