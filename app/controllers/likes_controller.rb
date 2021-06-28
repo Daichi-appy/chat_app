72d96b6531ad8c398c08b0994e651dd1d8431a52
daichi.aoki <daichi.es47@gmail.com>
2021-06-28 14:11:15 +0900

    修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,13 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.create(post_id: params[:post_id])
-    redirect_to root_path
+    redirect_to posts_path
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @like = current_user.likes.find_by(post_id: @post.id)
     @like.destroy
-    redirect_to root_path
+    redirect_to posts_path
   end
 end
