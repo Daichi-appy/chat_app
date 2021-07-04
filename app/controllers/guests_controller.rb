4c5de41c4a3f49d714ac61d1e4df40ad135fc066
daichi.aoki <daichi.es47@gmail.com>
2021-07-04 21:27:50 +0900

    chat機能修正, guestログイン実装

---
+++ app/controllers/guests_controller.rb
@@ -0,0 +1,10 @@
+class GuestsController < ApplicationController
+
+  def new_guest
+    user = User.find_or_create_by!(name: "ゲスト", email: "guest@mail.com") do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to root_path
+  end
+end
