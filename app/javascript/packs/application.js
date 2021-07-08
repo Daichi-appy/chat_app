ed1b6a0fce75181b4d4a11a2f7b06c1e01b5a723
daichi.aoki <daichi.es47@gmail.com>
2021-07-08 21:43:01 +0900

    いいねボタンajax実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,11 @@
 
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+require("jquery")
